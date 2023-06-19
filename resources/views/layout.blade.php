8066170ad4dfca1fcf0f7186a44933f5e601fd9c
DESKTOP-C8UM0C1\Ansh Panchal <panchal.ansh83@gmail.com>
2023-06-19 15:03:49 +0530

    Login commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+
+        @yield('content')
+
+        <div class="card bg-secondary text-white">
+            <div class="card-body">Copyright (c) 2022</div>
+        </div>
+    </div>
+</body>
+</html>
